2467eb526bcbdfe2a797d479f0b9a95010a14d5e
Glenn Van Mele <glennvanmele@gmail.com>
2015-11-30 16:46:20 +0100

    auth

--- routes/index.js
+++ routes/index.js
@@ -26,8 +26,9 @@ router.get('/posts', function(req, res, next){
 	});
 });
 
-router.post('/posts', function(req, res, next){
+router.post('/posts', auth, function(req, res, next){
 	var post = new Post(req.body);
+	post.author = req.payload.username;
 	post.save(function(err, post) {
 		if (err) {
 			return next;
@@ -79,6 +80,7 @@ router.put('/posts/:post/upvote', auth, function(req, res, next) {
 router.post('/posts/:post/comments', auth, function(req, res, next) {
 	var comment = new Comment(req.body);
 	comment.post = req.post;
+	comment.author = req.payload.username;
 
 	comment.save(function(err, comment){
 		if(err){ return next(err); }
