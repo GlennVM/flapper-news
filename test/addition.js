6b56a41eaf652c1ce6d213da9db35e016c05a254
Glenn Van Mele <glennvanmele@gmail.com>
2015-12-04 12:09:39 +0100

    Toevoegen test posts

--- test/addition.js
+++ test/addition.js
@@ -1,3 +1,38 @@
+var expect = require("chai").expect;
+var app = require("../app");
+var request = require('supertest');
+var agent = request.agent(app);
+
+describe("Init", function() {
+    it("starts a new testing env", function(done) {
+        expect("test").to.equal("test");
+        done();
+    });
+});
+
+describe("GET /posts", function() {
+    it('should respond with 200 in case of valid request', function() {
+        agent.get('/posts')
+            .send()
+            .end(function(err, res) {
+                if(err) {return done(err);}
+                var fetchedData = JSON.parse(res.text);
+                expect(fetchedData).to.be.an('array');
+                expect(fetchedData).to.not.empty;
+
+                var post = fetchedData[0];
+
+                if(post) {
+                    expect(post).to.have.all.keys("__v","_id","comments","upvotes");
+                    expect(post.comments).to.be.an('array');
+                    expect(post.upvotes).to.be.a('number');
+                }
+                done();
+            });
+    });
+});
+
+
 describe('true', function(){
 	it('should be true', function(done){
 		var val = true;
